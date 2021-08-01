e674fbc8195f6c21c67cfc34775734a032ecc919
Nero Lu <cbhb4509@gmail.com>
2021-08-01 13:22:15 +0800

    [重構]將登入、登出，抽取至共用函式

--- WebFormMiniSample/AccountingNote.Auth/AuthManager.cs
+++ WebFormMiniSample/AccountingNote.Auth/AuthManager.cs
@@ -46,10 +46,44 @@ namespace AccountingNote.Auth
 
             return model;
         }
-
+        /// <summary>登出 </summary>
         public static void Logout()
         {
             HttpContext.Current.Session["UserLoginInfo"] = null;
+        }
+        public static bool TryLogin(string account,string pwd,out string errorMsg)
+        {
+            //check empty
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(pwd))
+            {
+                errorMsg = "Account/PWD is required.";
+                return false;
+            }
+
+            //read db and check
+            var dr = UserInfoManager.GetUserInfoByAccount(account);
+
+            //check null
+            if (dr == null)       // 如果帳號不存在，導至登入頁
+            {
+                errorMsg = $"Account:{account} doesn't exists.";
+                return false;
+            }
+
+            // check account /pwd
+            //密碼需區分大小寫
+            if (string.Compare(dr["Account"].ToString(), account,true) == 0 && string.Compare(dr["PWD"].ToString(), pwd,false) == 0)
+            {
+                HttpContext.Current.Session["UserLoginInfo"] = dr["Account"].ToString();
+
+                errorMsg = string.Empty;
+                return true;
+            }
+            else
+            {
+                errorMsg = "Login fail. Please check Account / PWD.";
+                return false;
+            }
         }
     }
 }
--- WebFormMiniSample/AccountingNote/Login.aspx.cs
+++ WebFormMiniSample/AccountingNote/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using AccountingNote.Auth;
 using AccountNote.DBSources;
 
 namespace AccountingNote
@@ -25,36 +26,17 @@ namespace AccountingNote
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            string db_Account = string.Empty;
-            string db_Password = string.Empty;
-
             string inp_Account = this.txtAccount.Text;
             string inp_PWD = this.txtPWD.Text;
 
-            //check empty
-            if(string.IsNullOrWhiteSpace(inp_Account) || string.IsNullOrWhiteSpace(inp_PWD))
+            string msg;
+            if(!AuthManager.TryLogin(inp_Account, inp_PWD,out msg))
             {
-                this.ltlMsg.Text = "Account/PWD is required.";
+                this.ltlMsg.Text = msg;
                 return;
             }
 
-            var dr = UserInfoManager.GetUserInfoByAccount(inp_Account);
-
-            if (dr == null)       // 如果帳號不存在，導至登入頁
-            {
-                this.Session["UserLoginInfo"] = null;
-                Response.Redirect("/Login.aspx");
-                return;
-            }
-            db_Account = dr["Account"].ToString();
-            db_Password = dr["PWD"].ToString();
-            // check account /pwd
-            //密碼需區分大小寫
-            if (string.Compare(db_Account,inp_Account,true)==0 && string.Compare(db_Password, inp_PWD )==0)
-            {
-                this.Session["UserLoginInfo"] = dr["Account"].ToString();
-                Response.Redirect("/SystemAdmin/UserInfo.aspx");
-            }
+            Response.Redirect("/SystemAdmin/UserInfo.aspx"); 
         }
     }
 }
